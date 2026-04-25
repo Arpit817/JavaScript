f3d9d520cd02278e585e7ee85280e500c2739959
Arpit Anand Patel <arpitpatel7388@gmail.com>
2026-04-25 09:25:44 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+let name = "Arpit"  //string
+let age = 20  // integer
+let islogin = false //boolean
+ console.log(name);
+
+
+//primitive datatypes
+//number => 2 to power 20
+//int
+//string ""
+//boolean
+//null = standalone value
+// undefined 
+// symbol = unique
+
+//object
+
+console.log(typeof "Arpit");
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null); // object
