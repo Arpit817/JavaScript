d45f6e103dd6a57a78c03fd04ca35cac4033b632
Arpit Anand Patel <arpitpatel7388@gmail.com>
2026-04-25 11:28:41 +0000

    operations

--- Basics/ConversionOperation.js
+++ Basics/ConversionOperation.js
@@ -1,11 +1,11 @@
 let score = "24"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"45" = 45
 //"45vsrg" = NaN
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLogin = 1
 
 let booleanIslogin = Boolean(isLogin);
-console.log(booleanIslogin)
+// console.log(booleanIslogin)
 
 //1 = true; 0 = false
 // "" = false
@@ -23,5 +23,31 @@ console.log(booleanIslogin)
 let number = 40
 
 let stringNumber = String(number);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//  *****operations ****
+
+let value = 5
+
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Arpit"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 3);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
