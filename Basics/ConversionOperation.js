9d9e28c77151e2a0559631aa200b5f0295f3df8d
Arpit Anand Patel <arpitpatel7388@gmail.com>
2026-04-25 10:57:25 +0000

    conversion of datatype

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "24"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"45" = 45
+//"45vsrg" = NaN
+// true = 1; false = 0
+
+let isLogin = 1
+
+let booleanIslogin = Boolean(isLogin);
+console.log(booleanIslogin)
+
+//1 = true; 0 = false
+// "" = false
+//"arpit" = true
+
+let number = 40
+
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
